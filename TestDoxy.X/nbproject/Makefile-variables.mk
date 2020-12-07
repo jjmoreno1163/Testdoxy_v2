49a384a57cc7825ae5ac04d04d99d7ed89ca0364
juamoreno <juamoreno@ES-P611.etmlnet.local>
2020-12-07 09:43:06 +0100

    1ere version

---
+++ TestDoxy.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TestDoxy.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TestDoxy.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testdoxy.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testdoxy.x.tar
